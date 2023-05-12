f2262414cc644d7a54c6041e2316cdfae937fffd
Emiliano Chavez De La Torre <111912343+emilianochavezDLT@users.noreply.github.com>
2023-05-11 21:28:20 -0600

    Update mainSchedulio.py

--- mainSchedulio.py
+++ mainSchedulio.py
@@ -1,11 +1,12 @@
 #This is created by Emiliano Chavez De La Torre and Kenyou Teoh
 #This is the main file of Scedulio, it is the file that will be run to start the program
 
+#This is the main function of the program
 def main():
 
     #This is a test to see if the program is running
     print("Hello World")
-
+    
 
 
 
