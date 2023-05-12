7d88a1430f34b596606413692594288867809190
Emiliano Chavez De La Torre <111912343+emilianochavezDLT@users.noreply.github.com>
2023-05-11 21:13:38 -0600

    Create mainSchedulio.py

---
+++ mainSchedulio.py
@@ -0,0 +1,10 @@
+#This is created by Emiliano Chavez De La Torre and Kenyou Teoh
+#This is the main file of Scedulio, it is the file that will be run to start the program
+
+def main():
+
+
+if __name__ == "__main__":
+    main()
+
+
