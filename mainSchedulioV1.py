59fd88a9a2a47058a8ffea76f841c4de53992711
Gamer0813 <gamesplayer0813@gmail.com>
2023-05-12 09:23:49 -0600

    Create mainSchedulioV1.py

---
+++ mainSchedulioV1.py
@@ -0,0 +1,17 @@
+#This is created by Emiliano Chavez De La Torre and Kenyou Teoh
+#This is the main file of Scedulio, it is the file that will be run to start the program
+
+#This is the main function of the program
+def main():
+
+    #This is a test to see if the program is running
+    print("Hello World")
+    
+
+
+
+if __name__ == "__main__":
+    main()
+
+
+#This is a test for v1
