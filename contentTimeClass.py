58b6df52a7189d00901a4bb22c55d229ae582a36
DotCh2 <emilianochavezdelatorre@gmail.com>
2023-05-30 22:03:56 +0800

    contentTimeClass
    The FileContent class represents the content of a file and provides various methods to split and analyze the content. It is designed to handle files with specific patterns, including time intervals, keywords, and individual times

--- contentTimeClass.py
+++ contentTimeClass.py
@@ -2,32 +2,58 @@
 #This class is used to store the content and time of the schedule
 
 #This class is used to store the content and it is also the parent class of the time class
-class fileContent:
+class FileContent:
 
     def __init__(self, content):
         self.content = content
-    
-    #This is our setter function
+        self.timeIntervals = []
+        self.indivdualTimes = []
+        self.keyword = ""
+        self.keywords = []
+
+    #           Setters             #
+    #This is our setter function for setting the content of the file
     def setContent(self, content):
         self.content = content
 
-    #This is our getter function
+    #This is our setter function for setting the keyword of the file
+    def setKeyword(self, keyword):
+        self.keyword = keyword
+
+    #This function is used to add a time interval to the timeIntervals list
+    def addTimeInterval(self, timeInterval):
+        self.timeIntervals.append(timeInterval)
+    
+    #This function is used to add a keyword to the keywords list
+    #This is a just incase function if mulitple keywords pop up
+    def addKeywords(self, keyword):
+        self.keywords.append(keyword)
+
+    #####
+    #####
+    #This function is used to add a time to the indivdualTimes list from a time interval
+    def addIndivdualTime(self, timeInterval):
+        time = ""
+        #Add some logic here to get an induvdual time from a time interval
+        self.indivdualTimes.append(time)
+
+    #           Getters             #
+    #This is our getter function for the content of the file
     def getContent(self):
         return self.content
+    
+    #This is our getter function for the timeIntervals list
+    def getTimeIntervals(self):
+        return self.timeIntervals
+
+    def getKeyword(self):
+        return self.keyword
+    
+    def getKeywords(self):
+        return self.keywords
     
     #This function is used to print the content
     #We just have to use print(fileContent) to print the content
     def __str__(self):
         return self.content
     
-#This class is used to store the time and it is the child class of the content class
-class fileTime(fileContent):
-
-    def __init__(self, content, time):
-        #We use super() to inherit the content from the parent class
-        super().__init__(content)
-        #We set the time
-        self.time = time
-
-
-
