ded1bbdb826c8b298a19737a01800d56099d2f06
Emiliano Chavez De La Torre <111912343+emilianochavezDLT@users.noreply.github.com>
2023-08-07 23:19:27 -0600

    Update mainSchedulio.py

--- mainSchedulio.py
+++ mainSchedulio.py
@@ -1,21 +1,28 @@
 #This is created by Emiliano Chavez De La Torre, Kenyou Teoh, and Josh Miller
 #This is the main file of Scedulio, it is the file that will be run to start the program
 
+from Event import Event
 
 
 
 
-
-
-def skeleton():
-
+def displaySkeleton():
     #This is a skeleton for the program, it will be used to create the main program
     #This is a test to see if the program is running
     print("Hello World")
 
+
 def addToSchedule(employee, event):
+    print("Hello World")
+
+
 
 def removeFromSchedule(employee, event):
+    print("Hello World")
+
+
+
+
 
 
 #This is the main function of the program
