a7baaf66fa1b993cac75ec8d696fa66c7147a14b
Emiliano Chavez De La Torre <111912343+emilianochavezDLT@users.noreply.github.com>
2023-08-08 18:09:59 -0600

    Changes done in Main
    Display Skelaton Function

--- mainSchedulio.py
+++ mainSchedulio.py
@@ -1,26 +1,12 @@
-#This is created by Emiliano Chavez De La Torre, Kenyou Teoh, and Josh Miller
+#This is created by Emiliano Chavez De La Torre, Kenyou Teoh, Joshua Miller
 #This is the main file of Scedulio, it is the file that will be run to start the program
 
+#Importing the Employee class, and the Event class
+from Employee import Employee
 from Event import Event
 
 
-
-
 def displaySkeleton():
-    #This is a skeleton for the program, it will be used to create the main program
-    #This is a test to see if the program is running
-    print("Hello World")
-
-
-def addToSchedule(employee, event):
-    print("Hello World")
-
-
-
-def removeFromSchedule(employee, event):
-    print("Hello World")
-
-
 
 
 
@@ -31,8 +17,6 @@ def main():
     #This is a test to see if the program is running
     print("Hello World")
     
-
-
 
 if __name__ == "__main__":
     main()
