e223ecf883c8b2f054d89d62bcf7fa8563cea5f5
Emiliano Chavez De La Torre <111912343+emilianochavezDLT@users.noreply.github.com>
2023-08-09 19:32:06 -0600

    Update mainSchedulio.py

--- mainSchedulio.py
+++ mainSchedulio.py
@@ -2,17 +2,35 @@
 #This is the main file of Scedulio, it is the file that will be run to start the program
 
 import sys
-path_Beo = input("Enter the path to the BEO file: ")
-sys.path.append(path_Beo)
 
+#Since we don't have the BEO_Editor directory in the same directory as this file
+#We need to add the path to the BEO_Editor directory ourselves
+print("You need to input a path for the Beo Editor Directory")
+print("For Emiliano the diretory is /Users/Echav/Documents/ANC_Root/")
+print("For Kenyou the directory is (Your directory here)")
+print("For Josh the directory is (Your directory here)")
+print("If you know the directory then input (e), (k), or (j) for the corresponding person")
+
+path_Emiliano = "/Users/Echav/Documents/ANC_Root/"
+path_Kenyou = ""
+path_Josh = ""
+path_Input = input("Input the path here: ")
+if path_Input == "e":
+    path_Beo = path_Emiliano
+elif path_Input == "k":
+    path_Beo = path_Kenyou
+elif path_Input == "j":
+    path_Beo = path_Josh
+else:
+    path_Beo = path_Input
+sys.path.append(path_Beo)
 
 
 #Import the Employee class from the employee.py file
 from Employee import Employee
-
 #Import the Event class from the Event.py file. It's importing the class Event as a package
 from BEO_Editor import BEO
-from BEO_Editor.Event import Event
+from BEO_Editor import Event
 
 
 #The display_Skelaton function will display the skelaton of the schedule
@@ -27,7 +45,7 @@ def display_Skelaton(array_of_BEOs):
     #and dispaly the corresponding factors needed fore the skelaton
     for beo in array_of_BEOs:
         print(f"{beo.get_function_room()} {beo.get_event_class()}")
-        print(f"{beo.get_hour()}:{beo.get_minutes()}" " " * 5 + f"{beo.get_people()}")
+        print(f"{beo.get_hour()}:{beo.get_minutes()}     {beo.get_people()}")
 
 
 #This is the main function of the program
@@ -37,11 +55,11 @@ def main():
     print("Welcome to Schedulio!")
 
     #Calling an instance of an Event
-    my_event1 = Event(event_name="Wedding Reception", event_date="2023-08-15")
+    my_event1 = Event.Event(event_name="Wedding Reception", event_date="2023-08-15")
 
     #Calling an instance of a BEO
     # Creating an instance of the BEO class
-    my_beo1 = BEO(
+    my_beo1 = BEO.BEO(
         price=2500.00,
         hour=18,
         minutes=30,
@@ -53,12 +71,13 @@ def main():
         comments="Allergies: No nuts, vegetarian options required."
     )
 
-    my_event2 = Event(event_name="Group Breakfast", event_date="2023-08-15")
-    my_beo2 = BEO(
+    my_event2 = Event.Event(event_name="Group Breakfast", event_date="2023-08-15")
+    
+    my_beo2 = BEO.BEO(
         price=1500.00,
         hour=18,
         minutes=30,
-        people=100,
+        people=300,
         event=my_event2,
         menu_item="Filet Mignon",
         function_room="Foyer",
